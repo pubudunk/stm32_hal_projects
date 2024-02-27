d28c431b20c458362f734cf487887e05de7d435e
Pubudu Karunaratna <pubudu1@gmail.com>
2024-02-27 14:32:52 -0600

    011HAL_WFI_Button
    	Button press interrupt to send uart message and WFI instruction
    	to sleep (normal). Power saving 5.39mA vs 3.72

---
+++ 011HAL_WFI_Button/Core/Src/it.c
@@ -0,0 +1,19 @@
+/*
+ * it.c
+ *
+ *  Created on: Jan 21, 2024
+ *      Author: pubudukarunaratna
+ */
+#include <main.h>
+
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+
+	HAL_SYSTICK_IRQHandler();
+}
+
+void EXTI0_IRQHandler(void)
+{
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+}
