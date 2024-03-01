1b88bba389971b58ec195d313a85010e355e46fd
Pubudu Karunaratna <pubudu1@gmail.com>
2024-02-29 20:45:57 -0600

    013HAL_Current_Measurement_Stop_Mode - bug fix

--- 013HAL_Current_Measurement_Stop_Mode/Core/Inc/power_modes.h
+++ 013HAL_Current_Measurement_Stop_Mode/Core/Inc/power_modes.h
@@ -13,6 +13,6 @@ void PWR_StopMainRegFlashPwrDn(void);
 void PWR_StopLPRegFlashOn(void);
 void PWR_StopLPRegFlashPwrDn(void);
 void PWR_StopMainRegUnderDrvFlashPwrDn(void);
-void PWR_StopMainRegUnderDrvFlashPwrDn(void);
+void PWR_StopLPRRegUnderDrvFlashPwrDn(void);
 
 #endif /* INC_POWER_MODES_H_ */
